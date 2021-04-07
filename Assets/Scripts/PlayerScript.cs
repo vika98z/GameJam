c3e588f5d5bd348f293e41674d9c80c3f057a9f4
Mirradell <olya.shtejn@yandex.ru>
2021-04-07 23:48:55 +0300

    Done static room, movements of player with WASD, temp light

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Mathf.Clamp(Input.GetAxis("Horizontal") * 3 + transform.position.x, -13f, 13f);
+        var vertical = Mathf.Clamp(Input.GetAxis("Vertical") * 3 + transform.position.z, -12f, 13f);
+        transform.position = new Vector3(horizontal, 0, vertical);
+
+    }
+}
