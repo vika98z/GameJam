efef1b5261e843cf8c2a0e016df43aae28d6d06d
vika98z <vika98z@mail.ru>
2021-04-08 20:48:11 +0300

    camera and player movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  private const float YAngleMIN = 0.0f;
+  private const float YAngleMAX = 50.0f;
+  
+  [SerializeField] private Transform target;
+  [SerializeField] private Vector3 offset;
+  [SerializeField] private float smoothTime = 0.3f;
+
+  private Vector3 _velocity = Vector3.zero;
+
+  private float _currentX;
+  private float _currentY = 45.0f;
+  private float speedRotateX = 1;
+  private float speedRotateY = 1;  
+  
+  Vector3 playerPrevPos, playerMoveDir;
+  float distance;
+  
+  private void Start()
+  {
+    playerPrevPos = target.transform.position; 
+    distance = offset.magnitude;
+  }
+
+  private void Update()
+  {
+    _currentX += speedRotateX * Input.GetAxis("Mouse X");
+    _currentY -= speedRotateY * Input.GetAxis("Mouse Y");
+
+    _currentY = Mathf.Clamp(_currentY, YAngleMIN, YAngleMAX);
+  }
+  
+  private void LateUpdate()
+  {
+    Vector3 targetPosition = target.position + offset;
+    
+    var rotation = new Vector3(_currentY, _currentX, 0);
+    
+    //_camTransform.position = position + rotation * dir;
+    
+    //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+    //transform.eulerAngles = rotation;
+    //transform.LookAt(target);
+    
+    
+    playerMoveDir = target.transform.position - playerPrevPos;
+    playerMoveDir.Normalize();
+    transform.position = target.transform.position - playerMoveDir * distance;
+
+    transform.LookAt(target.transform.position);
+
+    playerPrevPos = target.transform.position;
+  }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 public class Player : MonoBehaviour 
 { 
   [SerializeField] private float speed; 
-  [SerializeField] private float turnSpeed; 
-  [SerializeField] private float gravity;
   [SerializeField] private float jumpHeight;
+  [SerializeField] private float turnSmoothTime = 0.1f;
+  [SerializeField] private Transform cam;
   
-  private CharacterController _controller; 
-  private Vector3 _moveDirection = Vector3.zero; 
- 
+  private CharacterController _controller;
+
   private Vector3 _playerVelocity;
   private bool _groundedPlayer;
   private float gravityValue = -9.81f;
+  private float turnSmoothVelocity;
   
   private void Awake() => 
     _controller = GetComponent<CharacterController>(); 
@@ -29,11 +29,18 @@ public class Player : MonoBehaviour
     if (_groundedPlayer && _playerVelocity.y < 0)
       _playerVelocity.y = 0f;
 
-    Vector3 move = new Vector3(horizontal, 0, vertical);
-    _controller.Move(move * Time.deltaTime * speed);
+    Vector3 direction = new Vector3(horizontal, 0, vertical);
 
-    if (move != Vector3.zero)
-      gameObject.transform.forward = move;
+    if (direction.magnitude >= .1f)
+    {
+      float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+      float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+      transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+      Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+      
+      _controller.Move(moveDir.normalized * Time.deltaTime * speed);
+    }
 
     if (Input.GetButtonDown("Jump") && _groundedPlayer)
       _playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
