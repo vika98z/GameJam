88406df25625c00d4acd559fe5a3379dbb47fb7f
vika98z <vika98z@mail.ru>
2021-04-10 02:45:33 +0300

    text with help

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PickableObject[] pickableObjects;
     [SerializeField] private Camera camera;
+    [SerializeField] private Text helpText;
+
+    public bool IsHelping = true;
     
     private void Awake()
     {
@@ -12,5 +16,10 @@ public class GameManager : MonoBehaviour
 
         foreach (var pickableObject in pickableObjects)
             pickableObject.MainCamera = camera;
+    }
+
+    public void SetHelpText(string message)
+    {
+        helpText.text = message;
     }
 }
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -8,7 +8,13 @@ public class PickUp : MonoBehaviour
   [SerializeField] private Transform objectHolder;
   [SerializeField] private PickableObject item;
   [SerializeField] private float pickUpDistance;
+  [SerializeField] private GameManager gameManager;
 
+  [Space] public string dropText = "Нажмите ЛКМ, чтобы бросить предмет";
+  public string pickUpText = "Нажмите Е, чтобы взять предмет";
+  public string connectText = "Нажмите ЛКМ, чтобы объединить предметы";
+  public string connectAndRotateText = "Крутите колесико мыши, чтобы вращать предмет. Нажмите ЛКМ, чтобы объединить предметы";
+  
   private bool _carryObject;
 
   public List<PickableObject> _highlightedObjectsList = new List<PickableObject>();
@@ -29,7 +35,19 @@ public class PickUp : MonoBehaviour
         var raycastedObject = hit.collider.gameObject.GetComponent<PickableObject>();
 
         if (raycastedObject != null && raycastedObject != item && raycastedObject.IsHighlightable)
+        {
+          if (CanConnect())
+          {
+            if (item.TypeOfDetail == DetailType.Cube)
+              gameManager.SetHelpText(connectText);
+            else
+              gameManager.SetHelpText(connectAndRotateText);
+          }
+          else if (CanPickUp())
+            gameManager.SetHelpText(pickUpText);
+          
           CheckObjectAndSetHighlight(raycastedObject);
+        }
       }
       else
         ClearHighlighted();
@@ -37,12 +55,9 @@ public class PickUp : MonoBehaviour
     else
       ClearHighlighted();
 
-    if (CanPickUp())
-    {
-      if (!_highlightedObjectsList.Last().IsConnected && _highlightedObjectsList.Last().IsHighlightable)
-        PickUpObject();
-    }
-    else if (CanConnect())
+    if (CanPickUp() && Input.GetKeyDown(KeyCode.E))
+      PickUpObject();
+    else if (CanConnect() && Input.GetMouseButton(0))
     {
       if (item.Connect(_highlightedObjectsList.Last()))
         Throw();
@@ -53,11 +68,11 @@ public class PickUp : MonoBehaviour
       Throw();
 
     bool CanPickUp() =>
-      Input.GetKeyDown(KeyCode.E) && _detectObject && !_carryObject;
+       _detectObject && !_carryObject && !_highlightedObjectsList.Last().IsConnected && _highlightedObjectsList.Last().IsHighlightable;
 
     bool CanConnect() =>
       item != null && _highlightedObjectsList.Count > 0 && item.IsConnectable
-      && Input.GetMouseButton(0) && _detectObject && _carryObject 
+      && _detectObject && _carryObject 
       && item != _highlightedObjectsList.Last() && _highlightedObjectsList.Last().IsHighlightable && _highlightedObjectsList.Last().IsConnected;
   }
 
@@ -94,6 +109,8 @@ public class PickUp : MonoBehaviour
       _carryObject = false;
 
       item = null;
+      
+      gameManager.SetHelpText("");
     }
   }
 
@@ -114,6 +131,11 @@ public class PickUp : MonoBehaviour
 
   private void ClearHighlighted()
   {
+    if (_carryObject)
+      gameManager.SetHelpText(dropText);
+    else 
+      gameManager.SetHelpText("");
+    
     _detectObject = false;
 
     if (_highlightedObjectsList.Count > 0)
