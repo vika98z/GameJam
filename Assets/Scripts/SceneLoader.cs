1de8befc6671416c032bfe1cc52b99c974ffeba8
vika98z <vika98z@mail.ru>
2021-04-12 02:55:13 +0300

    menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string loadLevel;
+
+    [SerializeField] private GameObject loadingScreen;
+    
+    public void Load()
+    {
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadAsync());
+    }
+
+    private IEnumerator LoadAsync()
+    {
+        var asyncOperation = SceneManager.LoadSceneAsync(loadLevel);
+
+        while (!asyncOperation.isDone)
+            yield return null;
+    }
+}
