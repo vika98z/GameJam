fa93dc195c0f096f3a5b4d795fef095163f5ccb8
vika98z <vika98z@mail.ru>
2021-04-12 05:45:56 +0300

    some environment

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,14 +9,23 @@ public class GameManager : MonoBehaviour
   [SerializeField] private Player player;
   [SerializeField] private Text helpText;
 
-  [SerializeField] private GameObject fog;
+  [SerializeField] private GameObject pointLights;
+  [SerializeField] private GameObject dirLight;
+  
+  [SerializeField] private GameObject bigPartOfCubes;
+  [SerializeField] private GameObject staticCube;
   
   public bool IsHelping = true;
 
+  private float _lightningXPosNormal = 140;
+  private float _lightningXPosNight = -90;
+  
   private void Awake()
   {
     Cursor.visible = false;
     Cursor.lockState = CursorLockMode.Locked;
+    
+    RenderSettings.fog = false;
 
     foreach (var pickableObject in pickableObjects)
     {
@@ -38,7 +47,7 @@ public class GameManager : MonoBehaviour
         {
           //bad - blue
           case 0:
-            fog.SetActive(true);
+            RenderSettings.fog = false;
             break;
           //good - red
           case 1:
@@ -50,9 +59,17 @@ public class GameManager : MonoBehaviour
       case 3:
         switch (PlayerPrefs.GetInt("Button"))
         {
+          //good - blue
           case 0:
+            bigPartOfCubes.SetActive(true);
+            staticCube.SetActive(false);
             break;
+          //bad - red
           case 1:
+            pointLights.SetActive(false);
+            var rotation = dirLight.transform.rotation;
+            rotation = Quaternion.Euler(_lightningXPosNight, rotation.y, rotation.z);
+            dirLight.transform.rotation = rotation;
             break;
         }
         break;
